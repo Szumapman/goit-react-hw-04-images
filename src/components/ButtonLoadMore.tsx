ee09c7bb9281f94a64d282df0379cdad301f14ae
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-11 11:57:59 +0100

    Update ButtonLoadMore.tsx

--- src/components/ButtonLoadMore.tsx
+++ src/components/ButtonLoadMore.tsx
@@ -1,13 +1,11 @@
-import { usePictures } from '../hooks/UsePictures';
 import css from './ButtonLoadMore.module.css'
 
-export const ButtonLoadMore = () => {
+export const ButtonLoadMore = ({ onClick }: { onClick: () => void }) => {
 
-    const {setPage} = usePictures();
 
     return (
         <div>
-            <button onClick={() => setPage(prevPage => prevPage + 1)} className={css.Button}>Load more</button>
+            <button onClick={onClick} className={css.Button}>Load more</button>
         </div>
     )
 }
