a1b9938b586cfa69ace03dce620770f2f27e4bbc
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-11 11:58:09 +0100

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,39 +1,18 @@
-import React, { useRef } from 'react';
 import { Picture } from "../interfaces/Picture";
 import css from './ImageGallery.module.css';
-import { usePictures } from '../hooks/UsePictures';
 
-const ImageItem = React.memo(({ picture, handlePictureClick, onLoad }: { picture: Picture, handlePictureClick: (image: Picture) => void, onLoad: () => void }) => {
-    return (
-        <li key={picture.id} className={css.ImageGalleryItem} onClick={() => handlePictureClick(picture)}>
-            <img src={picture.webformatURL} alt={picture.tags} onLoad={onLoad} className={css.ImageGalleryItemImage} />
-        </li>
-    );
-});
-
-export const ImageGallery = React.memo(() => {
-    const { pictures, setLoadedImages, handlePictureClick } = usePictures();
-    const loadedImageIds = useRef(new Set<number>());
-
-    // const handleImageLoad = (id: number) => {
-    //     if (!loadedImageIds.current.has(id)) {
-    //         loadedImageIds.current.add(id);
-    //         setLoadedImages(prev => prev + 1);
-    //     }
-    // };
-
+export const ImageGallery = ({ images, handleImageClick, onImageLoad }: 
+    { images: Picture[], handleImageClick: (image: Picture) => void, onImageLoad: () => void }) => {
+    
     return (
         <div>
             <ul className={css.ImageGallery}>
-                {pictures.map(picture => (
-                    <ImageItem
-                        key={picture.id}
-                        picture={picture}
-                        handlePictureClick={handlePictureClick}
-                        onLoad={() => setLoadedImages(prev => prev + 1)}
-                    />
+                {images.map(image => (
+                    <li key={image.webformatURL} className={css.ImageGalleryItem} onClick={() => handleImageClick(image)}>
+                        <img src={image.webformatURL} alt={image.tags} onLoad={onImageLoad} className={css.ImageGalleryItemImage} />
+                    </li>
                 ))}
             </ul>
         </div>
-    );
-});
+    )   
+}
