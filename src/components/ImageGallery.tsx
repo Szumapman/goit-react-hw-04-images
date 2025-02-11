dcd5794513856cdfaa67487d3e33989bf4d3ee47
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-11 10:52:33 +0100

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,39 +1,38 @@
-import React, { useMemo } from "react";
-import { usePictures } from "../hooks/UsePictures";
-import css from './ImageGallery.module.css';
+import React, { useRef } from 'react';
 import { Picture } from "../interfaces/Picture";
+import css from './ImageGallery.module.css';
+import { usePictures } from '../hooks/UsePictures';
 
 const ImageItem = React.memo(({ picture, handlePictureClick, onLoad }: { picture: Picture, handlePictureClick: (image: Picture) => void, onLoad: () => void }) => {
     return (
-        <li key={picture.webformatURL} className={css.ImageGalleryItem} onClick={() => handlePictureClick(picture)}>
+        <li key={picture.id} className={css.ImageGalleryItem} onClick={() => handlePictureClick(picture)}>
             <img src={picture.webformatURL} alt={picture.tags} onLoad={onLoad} className={css.ImageGalleryItemImage} />
         </li>
     );
 });
 
 export const ImageGallery = React.memo(() => {
-    
     const { pictures, setLoadedImages, handlePictureClick } = usePictures();
+    const loadedImageIds = useRef(new Set<number>());
 
-    const handleImageLoad = () => {
-        setLoadedImages(prev => prev + 1);
-    };
+    // const handleImageLoad = (id: number) => {
+    //     if (!loadedImageIds.current.has(id)) {
+    //         loadedImageIds.current.add(id);
+    //         setLoadedImages(prev => prev + 1);
+    //     }
+    // };
 
-    const imageItems = useMemo(() => {
-        return pictures.map(picture => (
-            <ImageItem
-                key={picture.webformatURL}
-                picture={picture}
-                handlePictureClick={handlePictureClick}
-                onLoad={handleImageLoad}
-            />
-        ));
-    }, [pictures, handlePictureClick]);
-    
     return (
         <div>
             <ul className={css.ImageGallery}>
-                {imageItems}
+                {pictures.map(picture => (
+                    <ImageItem
+                        key={picture.id}
+                        picture={picture}
+                        handlePictureClick={handlePictureClick}
+                        onLoad={() => setLoadedImages(prev => prev + 1)}
+                    />
+                ))}
             </ul>
         </div>
     );
